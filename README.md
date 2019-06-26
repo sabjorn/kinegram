493b9300c61e83752e1764915edd2a012d4719b1
sabjorn <info@sabjorn.net>
2019-06-25 20:06:16 -0700

    Adds all calculations for being able to generate animations
    Code is now able to generate animation (background and interference layer) with the interference layer being properly scaled based on the ratio of distance from viewer to background and viewer to foreground (parallax ratio).
    
    Code also shows using the parallax calculation to set the X (viewer movement) with a known P (animation period). This stuff will not likely be placed directly in the `Kinegram` class.
    
    Specifics:
    * added method for setting pixel width of kine object
    * Moved pixel width setting to instantiation of object.
    * added setting of parallax ratio with method (`parallaxRatio` is now set correctly)
    * modified overlay calculations to properly use parallax value
    
    Addtionally:
    * updates README to fully document all calculations
    * adds diagram for parallax calculations

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ See: https://en.wikipedia.org/wiki/Visual_angle
 Likely what is most important is to maintain the equality of the total stripe (black and clear) being the same as the total animation length.
 
 ## Calculations
+### Visual Angle
 ```
 V = 2arctan(S/2D)
 ```
@@ -35,4 +36,79 @@ since the `visual angle` is a constant for both objects:
 
 ```
 S1/D1 = S2/D2
+```
+
+#### Example
+Thus, an object of `0.01m` at a distance of `1m` will look to be the same size as an object of `0.02m` at a distance of `2m`.
+
+```
+0.01/1 = 0.02/2
+```
+
+#### Usage
+##### Image Scaling
+When prototyping an image, it should work well to test at a smaller size (single sheet of paper). Then, when scaling the image up there are two components which will have to be considered.
+
+When the image is scaled to be larger, the distance from viewer to image will be scaled. So, for a `20x28cm` image at `1m` scaled by a factor of 10:
+
+```
+20x28/1 = 200x280/D2
+D2 = 10
+```
+
+Second, the pixel width for the interference pattern will also have to be scaled. So, if a `1px` width looks good at `1m`, and the image is scaled by 10, the width will also have to be scaled by 10 (i.e. `10px` width).
+
+##### Front-to-Back Scaling
+The interference pattern is presumed to be overlayed directly over top of the image. However, for the parallax effect to work the interference pattern will be moved closer to the viewer. Thus, the pixel width will have to be increased proportional to this change of distance.
+
+For example, if the interference pattern is `10px` at `10m` but the image is shifted toward the viewer `1m`, it will appear to be larger and the pixel width will have to be scaled:
+
+```
+10px/10m = S2/9m
+S2 = 9px
+```
+
+**Note**: This pixel width will only need to be calculated once since the above scaling and the distance offset can all be considered one step. So, for the above examples, a `1px` width at `9m` will be `9px`, regardless of what stage it is calculated.
+
+### Animation Period
+The parallax effect works because of the `visual angle` effect discussed above. An object closer to the viewer moves at a different rate than one farther away. The image below gives an intuition of how relative to an object closer to the viewer (constantly kept in the centre of view) the background will move a larger amount:
+
+![Parallax Diagram No. 1](https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Parallax_Example.svg/1920px-Parallax_Example.svg.png)
+
+For the purposes of creating an animation with a set period, the fixed point will be considered a point on the interlace image (effectively 1 of the dark pixel columns). This allows the calculation of the viewer's travel distance in relation to the period of the animation (all calculated based on distances).
+
+The diagram below shows the relationship between the `animation period` (in meters, called `P` in the diagram) and the distance the viewer must walk (in meters, called `X` in the diagram):
+
+![Parallax Diagram No. 2](./figures/parallax.svg)
+
+The relationship of the elements above operates the same as with the `visual angle` calculations above:
+
+```
+X/D1 = P/D2
+```
+
+These 4 elements can be optimized for the strongest effect given some constraints.
+
+**NOTE**: The period of the animation is the number of frames times the width of the interlace pattern.
+
+#### Examples
+##### Known viewer walking distance and image distances
+If a viewer will have room for a `3m` viewer walking distance (x). The background image is `11m` away with the interference pattern `10m` away (i.e. background and interference pattern separated by `1m`):
+
+```
+D1 = 10
+D2 = (11 - 10)
+P = (X * D2) / D1
+p = (3 * 1) / 10
+p = 0.3m
+```
+
+**Note**: In this example, it is fairly obvious that a `9m` to `10m` distance is insufficient for an animation since the period is huge.
+
+##### Known viewer walking distance, known animation period, known D1
+If a viewer will have room for a `3m` viewer walking distance (x). The interlace image is `10m` away from the ivewer with the animation period is `.5m`:
+```
+D2 = (p * D1)/x
+D2 = (.5 * 10)/3
+D1 = 1.67m
 ```
--- kinegram/kinegram.py
+++ kinegram/kinegram.py
@@ -19,10 +19,19 @@ def autolog(logger, message):
     ))
 
 class Kinegram(object):
-    """A class for generating Kinegrams"""
-    def __init__(self, rotation=0):
+    """ A class for generating Kinegrams
+            pxlWidth = width of each interlace "frame" in pixels
+            rotation = angle to rotate image overlay (in degrees)
+            backgroundDistance = distance from viewer to background image
+            overlayDistance = distance from viewer to overlay (interference) image
+    """
+    def __init__(self, pxlWidth=1, rotation=0, backgroundDistance=1, overlayDistance=1):
         self.logger = logging.getLogger("kinegram")
         self.logger.debug("kinegram created")
+
+        self.dpm = (300 / 0.0254) # const
+
+        self.setPixelWidth(pxlWidth)
 
         self.rotation = rotation
 
@@ -32,6 +41,8 @@ class Kinegram(object):
         self.interHeight = np.nan
         self.interDepth = np.nan
         self.dtype = 'uint8'
+
+        self.setParallaxRatio(overlayDistance, backgroundDistance)
 
         self.interlaceWidth = None
         self.overlay = None
@@ -52,18 +63,17 @@ class Kinegram(object):
         self.dtype = img.dtype
         self.bg_im.append(img)
 
-    def generateInterlace(self, pxl_width=1, orientation=0):
+    def generateInterlace(self, orientation=0):
         """ Generates interlaced background image
-            pxl_width = width of each interlace "frame" in pixels
             orientation = direction of interlace
 
-            Note: crops input images to be even multiple of pxl_width
+            Note: crops input images to be even multiple of self.pxlWidth
             Note orientation currently does nothing
         """
         num_imgs = len(self.bg_im)
-        stepsize = pxl_width * num_imgs
+        stepsize = self.pxlWidth * num_imgs
         cropWidth = int(np.floor(self.interWidth / stepsize) * stepsize)
-        self.interlaceWidth = pxl_width * num_imgs
+        self.interlaceWidth = self.pxlWidth * num_imgs
 
         self.interlaced = np.zeros(
             (self.interHeight, cropWidth, self.interDepth),
@@ -72,25 +82,55 @@ class Kinegram(object):
         
         for i, img in enumerate(self.bg_im):
             img_crop = img[:,:cropWidth, :]
-            for j in np.arange(pxl_width):
-                self.interlaced[:, ((i * pxl_width)+j)::num_imgs*pxl_width, :] = img_crop[:, j::stepsize, :]
+            for j in np.arange(self.pxlWidth):
+                self.interlaced[:, ((i * self.pxlWidth)+j)::num_imgs*self.pxlWidth, :] = img_crop[:, j::stepsize, :]
 
     def generateOverlay(self, overlap):
         """ Generates foreground image (interference image)
-            overlap = [0, 1] pxl_width of opaque vs transpartent section
+            overlap = [0, 1] self.pxlWidth of opaque vs transpartent section
         """
         if self.interlaceWidth is None:
             raise Exception('you must generateInterlace before you can generate the Front')
 
-        overlap_actual = int(self.interlaceWidth * overlap) # no of pixels
-        overlay_modual = np.zeros((self.interHeight, self.interlaceWidth, 4), dtype=self.dtype)
+        interlaceWidthAdjusted = int(self.interlaceWidth * self.parallaxRatio)
+        if(interlaceWidthAdjusted < 1):
+            raise Exception('interlace width is sub 1px. Either increase pxlWidth or change parallax ratio')
+        overlap_actual = int(interlaceWidthAdjusted * overlap) # no of pixels
+        overlay_modual = np.zeros((self.interHeight, interlaceWidthAdjusted, 4), dtype=self.dtype)
         overlay_modual[:, 0:overlap_actual, 3] = 255 # opacity up
 
         # stack overlaps
-        stack_amount = int(self.interlaced.shape[1] / self.interlaceWidth) - 1
+        stack_amount = int(self.interlaced.shape[1] / interlaceWidthAdjusted) - 1
         self.overlay = np.copy(overlay_modual)
         for i in np.arange(stack_amount):
             self.overlay = np.hstack((self.overlay, overlay_modual))
+
+    def getAnimationPeriod(self):
+        """ 
+            Returns the animation period in meters
+        """
+        return (self.interlaceWidth / self.dpm)
+
+    def setParallaxRatio(self, overlayDistance, backgroundDistance):
+        """ 
+            set the Parallax Ratio used to calculate the pixel width
+            of the interlace image. This accounts for the effects of parallax
+
+            overlayDistance = distance from viewer to interference overlay
+            backgroundDistance = distance from viewer to the background animation
+
+            NOTE: must regenerate overlay after setting this
+        """
+        self.parallaxRatio = overlayDistance / backgroundDistance
+
+    def setPixelWidth(self, pxlWidth):
+        """
+            set the Pixel Width for animation frame
+            pxlWidth = number of pixels wide for animation frame
+
+            NOTE: after this is set, generateInterlace() must be run again
+        """ 
+        self.pxlWidth = max(pxlWidth, 1)
 
     def save(self, filename, directory="./"):
         """ exports interlaced and overlay as png images
@@ -99,9 +139,29 @@ class Kinegram(object):
         Image.fromarray(self.overlay).save("{0}{1}_overlayed.png".format(directory, filename))
 
 if __name__ == '__main__':
-    kine = Kinegram()
+    kine = Kinegram(pxlWidth=3)
     for i in np.arange(0, 9, 4):
         kine.loadImage("./examples/dance/dance00{0}.png".format(i+1))
 
-    kine.generateInterlace(pxl_width=3)
-    kine.generateOverlay(.75)
+    kine.generateInterlace()
+    kine.generateOverlay(.5)
+
+    ## calculate the viewer movement necessary for 1 animation cycle
+    """
+        X/D1 = P/D2
+        X = viewer movement distance (m), i.e. how far viewer has to move to see animation
+        P = Animation period (m)
+        D1 = Distance from foreground to background
+        D2 = Distance from viewer to foreground
+    """
+    backgroundDistance = 1
+    overlayDistance = D1 = .9
+    D2 = backgroundDistance - D1 
+    """
+        X = P * D1 / D2
+    """
+    viewerMovementDistance = kine.getAnimationPeriod() * D1 / D2
+    
+    ## NOTE generateOverlay needs a scaling parameter for generating the offsets for distance from background. Should be possible to do after as well
+
+    # use equation `p/D1 = x/D2` to define the image sizes.
